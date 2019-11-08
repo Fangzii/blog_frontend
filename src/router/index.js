265bb1f499587f5a7bc8e9e57295b8b45a422fa8
fangzicheng <fangzicheng@eastwestec.com>
2019-11-09 03:47:58 +0800

    url 处理

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import { constantRouterMap } from '@/config/router.config'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
