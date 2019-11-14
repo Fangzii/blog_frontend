f8cae487e43d366123c9ada28fb439b35f3df081
fangzicheng <fangzicheng@eastwestec.com>
2019-11-14 17:58:17 +0800

    侧栏初始化错误

--- src/core/bootstrap.js
+++ src/core/bootstrap.js
@@ -16,7 +16,7 @@ import {
 import config from '@/config/defaultSettings'
 
 export default function Initializer () {
-  store.commit('SET_SIDEBAR_TYPE', Vue.ls.get(SIDEBAR_TYPE, true))
+  store.commit('SET_SIDEBAR_TYPE', Vue.ls.get(SIDEBAR_TYPE, false))
   store.commit('TOGGLE_THEME', Vue.ls.get(DEFAULT_THEME, config.navTheme))
   store.commit('TOGGLE_LAYOUT_MODE', Vue.ls.get(DEFAULT_LAYOUT_MODE, config.layout))
   store.commit('TOGGLE_FIXED_HEADER', Vue.ls.get(DEFAULT_FIXED_HEADER, config.fixedHeader))
