0727a135ae14c09a61b5f4a077f075704862f2ad
fangzicheng <fangzicheng@eastwestec.com>
2019-11-18 15:48:57 +0800

    用户信息完善

--- src/components/tools/Logo.vue
+++ src/components/tools/Logo.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="informatization" :style="`min-height: ${hidden? '0' : '200'}px;${hidden? 'padding: 10px': 'padding: 20px'};`">
-    <div class="base_informatization">
-      <a-avatar :size="hidden? 38 : 52" icon="user" :src="logo" class="_logo"/>
-      <!-- <transition name="fade"> -->
-        <div v-if="!hidden">
+  <div class="informatization" :style="`min-height: ${hidden? '0' : '0'}px;${hidden? 'padding: 10px': 'padding: 20px'};`">
+    <div class="base_informatization" :style="`${hidden? 'margin-top: 30px' : ''};`">
+      <a-avatar :size="hidden? 38 : 52" icon="user" :src="logo" class="_logo" :style="`right: ${open?'-102' : '0'}px;top: 52px;`" v-show="!hidden" @click="open = !open"/>
+      <a-avatar :size="hidden? 38 : 52" icon="user" :src="logo" class="_logo" v-if="hidden"/>
+      <div class="switch_demo" @click="open = !open" v-show="!hidden">
+        <span class="switch_begin"></span>
+        <span class="switch_end"></span>
+      </div>
+      <transition>
+        <div v-if="!hidden && open" class="information_card">
           <div v-for="(item, index) in userInfoArr" :key="index">
             <a-icon :type="`${item.type}`" class="icon"/>
             <span>{{item.show}}</span>
           </div>
         </div>
-      <!-- </transition> -->
+      </transition>
     </div>
   </div>
 </template>
@@ -51,7 +56,8 @@ export default {
         github_url: 'github',
         link: 'link'
       },
-      logo: null
+      logo: null,
+      open: false
     }
   },
   methods: {
@@ -83,13 +89,12 @@ export default {
 <style lang="less">
 .informatization {
   transition: all .1s;
-  // color: #dad4d4 !important;
   filter: invert(100%);
   padding: 20px;
 }
 
 .base_informatization {
-  margin-top: 30px;
+  // margin-top: 30px;
   transition: all 300ms ease;
 }
 
@@ -98,8 +103,62 @@ export default {
 }
 
 ._logo {
+  transition: all .3s;
+  z-index: 100;
   filter: invert(100%) !important;
-  margin-bottom: 20px !important;
+}
+
+.switch {
+  &_demo {
+    cursor: pointer;
+    background: #d6d6d6;
+    transition: all .3s;
+    width: 50%;
+    height: 52px;
+    left: 23px;
+    margin-bottom: 20px !important;
+    position: relative;
+    opacity:0.3;
+  }
+
+  &_demo:hover {
+    opacity:1;
+  }
+
+  &_begin {
+    background: #d6d6d6;;
+    width: 52px;
+    height: 52px;
+    border-radius: 50%;
+    float: left;
+    right: 23px;
+    position: relative;
+  }
+
+  &_end {
+    background: #d6d6d6;
+    border-radius: 50%;
+    width: 52px;
+    height: 52px;
+    position: relative;
+    left: 23px;
+    float: right;
+  }
+}
+
+.information_card {
+  white-space: nowrap;
+  text-overflow:ellipsis;
+  overflow:hidden;  
+}
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+  // transform: translateX(100px);
+}
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.4s ease;
 }
 
 </style>
