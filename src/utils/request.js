9fe23138d10d2ee0c29d55e9573bed2a539ed534
fangzicheng <fangzicheng@eastwestec.com>
2019-11-21 10:23:29 +0800

    临时更改

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,7 +11,7 @@ import {
 
 // 创建 axios 实例
 const service = axios.create({
-  baseURL: 'http://3.114.86.155/api', // api base_url
+  baseURL: 'http://18.176.11.2/api', // api base_url
   timeout: 6000 // 请求超时时间
 })
 
